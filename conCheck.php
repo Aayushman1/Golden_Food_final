d0c556b9cefe67dca9dc016526b3048c7be72d8c
Aayushman Sinha <aayushmansinha09@gmail.com>
2023-05-09 21:53:05 +0200

    1st Commit

--- conCheck.php
+++ conCheck.php
@@ -1,13 +1,28 @@
-<?php
-    $servername="localhost";
-    $username="Aayushman";
-    $password="Aayushman1234$";
-    $dbName="golden_food";
-    $db=new mysqli($servername, $username, $password, $dbName);
+<!DOCTYPE html>
+<html lang="en">
 
-    if($db->connect_error){
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "Aayushman";
+    $password = "Aayushman1234$";
+    $dbName = "golden_food";
+    $db = new mysqli($servername, $username, $password, $dbName);
+
+    if ($db->connect_error) {
         die("<h1>Verbindung zum Server fehlgeschlagen!</h1><h2> Bitte für erneuten Versuch Seite neuladen <br> Beim bestehen des Problems den Administrator kontaktieren!</h2>");
-    }else{
+    } else {
         header("Location: loginScreen.html");
         exit();
     }
+    ?>
+</body>
+
+</html>
