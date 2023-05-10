2a61fcf3c737cf90f2ff532dfd00329d85caebbf
Aayushman Sinha <aayushmansinha09@gmail.com>
2023-05-10 11:30:46 +0200

    2nd Commit

--- conCheck.php
+++ conCheck.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
     <?php
+    header('Content-Type: text/html; charset=utf-8');
+
     $servername = "localhost";
     $username = "Aayushman";
     $password = "Aayushman1234$";
@@ -23,6 +14,3 @@
         exit();
     }
     ?>
-</body>
-
-</html>
